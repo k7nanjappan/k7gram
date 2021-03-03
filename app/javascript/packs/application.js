076cc7143943c214096d0ea8f3f823ed4dbacbce
Kesavan Nanjappan <k7nanjappan@gmail.com>
2021-03-03 20:24:34 +0530

    First Week Primary

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,77 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+
+//= require jquery3
+
+//= require rails-ujs
+//= require activestorage
+
+//= require dropzone
+//= require_tree .
+//= require toastr
+
+
+//import Rails from "@rails/ujs"
+//import Turbolinks from "turbolinks"
+//import * as ActiveStorage from "@rails/activestorage"
+//import "channels"
+
+import "bootstrap";
+import "../stylesheets/application";
+import Dropzone from "dropzone";
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+//require("packs/custom")
+//require("packs/posts.js")
+//require("dropzone").start()
+import './src/application.scss';
+import toastr from 'toastr/toastr';
+window.toastr = toastr
+//global.toastr = require("toastr")
+
+
+Dropzone.autoDiscover = false;
+
+$(document).ready(function() {
+	$(".upload-images").dropzone( {
+		addRemoveLinks: true,
+		maxFilesize: 1,
+		autoProcessQueue: false,
+		uploadMultiple: true,
+		parallelUploads: 100,
+		maxFiles: 100,
+		paramName: "images",
+		previewsContainer: ".dropzone-previews",
+		clickable: ".upload-photos-icon",
+		thumbnailWidth: 100,
+		thumbnailHeight: 100,
+
+		init: function() {
+			var myDropzone = this;
+
+
+			this.element.querySelector("input[type=submit]").addEventListener("click", function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				myDropzone.processQueue();
+			});
+
+			this.on("successmultiple", function(files, response) {
+				window.location.reload();
+			});
+
+			this.on("errormultiple", function(files, response) {
+				toastr.error(response);
+			});
+		}
+	})
+});
+
+//Rails.start()
+//Turbolinks.start()
+//ActiveStorage.start()
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  #get 'pages/home'
+  root :to =>'posts#index'
+
+  devise_for :users,
+  path: '',
+  path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+  controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+
+  #get '/users/:id', to: 'users#show '
+
+  resources :users, only: [:show]
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

bba2a357fd7bc46e0b66c8b19b74132998a73838
Kesavan Nanjappan <k7nanjappan@gmail.com>
2021-03-11 21:12:43 +0530

    probable locale fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   #root to: redirect("/#{I18n.locale}/posts/")
   #root :to => redirect("/#{I18n.locale}/posts")
    #root :to =>'posts#index'
-   root :to =>'pages#home'
-   #root :to =>'posts#index'
+   #root :to =>'pages#home'
+   root :to =>'posts#index'
    #root :to =>'users#show'
 
 

477d47e936d49b3a0506184049e4fae99caa5e27
k7nanjappan <k7nanjappan@gmail.com>
2021-03-11 23:35:58 +0530

    Update cloudinary.rb

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = "dr8v7mpnl"
-  config.api_key = "653846228531153"
-  config.api_secret = "EieHJ62yzYx0W5x_XsTBIoE8lhI"
+  config.cloud_name = ""
+  config.api_key = ""
+  config.api_secret = ""
   config.cdn_subdomain = true
 end
